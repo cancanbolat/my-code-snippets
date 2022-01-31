6add5d7905a19ec2135f2bb0c959cb359e974341
Can Canbolat <65857296+cancanbolat@users.noreply.github.com>
2022-01-31 17:59:21 +0300

    Add project files.

---
+++ SnippetTestsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Blog.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SnippetTestsController : ControllerBase
+    {
+        //getasync
+        [HttpGet]
+        public async System.Threading.Tasks.Task<IActionResult> EndpointName1()
+        {
+
+
+            return Ok();
+        }
+
+        //getparamsasync
+        [HttpGet("{example}")]
+        public async Task<IActionResult> EndpointName2(example)
+        {
+
+
+            return Ok();
+        }
+
+        //postasync
+        [HttpPost]
+        public async Task<IActionResult> EndpointName3([FromBody] Model model)
+        {
+
+
+            return Created("", model);
+        }
+
+
+    }
+}
